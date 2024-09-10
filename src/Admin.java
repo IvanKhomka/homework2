3947e5d5814f97783034516247a8f7a783db4f8c
IvanKhomka <khomichivan03@gmail.com>
2024-09-10 15:36:04 +0200

    created Admin class derived from User class

---
+++ src/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends User {
+    @Override
+    public void printRole() {
+        System.out.println("I am an Admin.");
+    }
+
+    public void checkTicket(Ticket ticket) {
+        System.out.println("Admin checking ticket: " + ticket);
+    }
+}
