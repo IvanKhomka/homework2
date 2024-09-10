53218ca74475e8d46eff841ec49fe5acf77c0435
IvanKhomka <khomichivan03@gmail.com>
2024-09-10 15:40:00 +0200

    getTicketsByStadiumSector(char sector) method is added to the TicketService class. It loops through the stored tickets and checks if the stadiumSector of each ticket matches the provided sector. Also added demonstrations of all functionalities, including the use of polymorphism.

--- src/TicketService.java
+++ src/TicketService.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketService {
 
     private Ticket[] tickets;
@@ -5,26 +8,34 @@ public class TicketService {
 
     public TicketService() {
         tickets = new Ticket[10];
-        ticketCount;
     }
+
     public static void main(String[] args) {
-        Ticket fullTicket = new Ticket("01","ArizonaHall",118,142354315,
-                true,'C',3.421, 40.0);
-        Ticket emptyTicket = new Ticket("0","GardenHall",176,1132341321,
-                false,'A',0.0, 40.0);
-        Ticket limitedTicket = new Ticket("GardenHall",177,1132341321);
+        Ticket ticket = new Ticket("1", "ArizonaHall", 118, 142354315,
+                true, 'C', 3.421, 40.0);
+        Ticket emptyTicket = new Ticket("0", "GardenHall", 176, 1132341321,
+                false, 'A', 0.0, 40.0);
+        Ticket limitedTicket = new Ticket("GardenHall", 177, 1132341321);
 
-
-        fullTicket.setPrice(45.24);
+        ticket.setPrice(45.24);
         emptyTicket.setPrice(0.0);
         limitedTicket.setPrice(30.0);
 
+        Client client = new Client();
+        Admin admin = new Admin();
+        client.printRole();
+        admin.printRole();
+        client.getTicket(ticket);
+        admin.checkTicket(ticket);
+        ticket.shareByPhone();
+        ticket.shareByEmail();
 
-        System.out.println(fullTicket);
+        System.out.println(ticket);
         System.out.println(emptyTicket);
         System.out.println(limitedTicket);
 
     }
+
     private boolean addTicket(Ticket ticket) {
         if (ticketCount < tickets.length) {
             tickets[ticketCount] = ticket;
@@ -33,7 +44,8 @@ public class TicketService {
         }
         return false;
     }
-    private Ticket getTicketById(String id) {
+
+    private Ticket getTicketById(int id) {
         for (int i = 0; i < ticketCount; i++) {
             if (tickets[i].getId().equals(id)) {
                 return tickets[i];
@@ -41,4 +53,15 @@ public class TicketService {
         }
         return null;
     }
+
+    public List<Ticket> getTicketsByStadiumSector(char sector) {
+        List<Ticket> result = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            if (tickets[i].getStadiumSector() == sector) {
+                result.add(tickets[i]);
+            }
+        }
+        return result;
+    }
 }
+
