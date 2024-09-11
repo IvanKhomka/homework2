fa9e204ececdb4a5a6ea8200c536087d3bf64b07
IvanKhomka <khomichivan03@gmail.com>
2024-09-11 10:55:28 +0200

    used casting and changed commented lines

--- src/TicketService.java
+++ src/TicketService.java
@@ -11,41 +11,51 @@ public class TicketService {
     }
 
     public static void main(String[] args) {
-        Ticket ticket = new Ticket("1", "ArizonaHall", 118, 142354315,
+        TicketService ticketService = new TicketService();
+        Ticket fullTicket = new Ticket("1", "ArizonaHall", 118, 142354315,
                 true, 'C', 3.421, 40.0);
         Ticket emptyTicket = new Ticket("0", "GardenHall", 176, 1132341321,
                 false, 'A', 0.0, 40.0);
         Ticket limitedTicket = new Ticket("GardenHall", 177, 1132341321);
 
-        ticket.setPrice(45.24);
+        ticketService.addTicket(fullTicket);
+        ticketService.addTicket(emptyTicket);
+        ticketService.addTicket(limitedTicket);
+        fullTicket.setPrice(45.24);
         emptyTicket.setPrice(0.0);
         limitedTicket.setPrice(30.0);
 
-        Client client = new Client();
-        Admin admin = new Admin();
+        User client = new Client();
+        User admin = new Admin();
         client.printRole();
         admin.printRole();
-        client.getTicket(ticket);
-        admin.checkTicket(ticket);
-        ticket.shareByPhone();
-        ticket.shareByEmail();
+        ((Client) client).getTicket(fullTicket);
+        ((Admin) admin).checkTicket(fullTicket);
+        fullTicket.shareByPhone();
+        fullTicket.shareByEmail();
 
-        System.out.println(ticket);
+        System.out.println(fullTicket);
         System.out.println(emptyTicket);
         System.out.println(limitedTicket);
 
+        char sectorToSearch = 'A';
+        List<Ticket> ticketsInSectorA = ticketService.getTicketsByStadiumSector(sectorToSearch);
+        System.out.println("Tickets in Stadium Sector '" + sectorToSearch + "':");
+        for (Ticket ticket : ticketsInSectorA) {
+            ticket.print();
+        }
     }
 
-    private boolean addTicket(Ticket ticket) {
+    private boolean addTicket(Ticket fullTicket) {
         if (ticketCount < tickets.length) {
-            tickets[ticketCount] = ticket;
+            tickets[ticketCount] = fullTicket;
             ticketCount++;
             return true;
         }
         return false;
     }
 
-    private Ticket getTicketById(int id) {
+    private Ticket getTicketById(String id) {
         for (int i = 0; i < ticketCount; i++) {
             if (tickets[i].getId().equals(id)) {
                 return tickets[i];
